0618e6403a149119f6c40e41e91f4fde550c69e6
Alex Santos <devalexsantos@gmail.com>
2023-12-20 12:01:12 -0300

    fixing layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer(){
     return(
-        <footer className="flex flex-col items-center mt-8 justify-center p-4 text-center">
+        <footer className="flex flex-col items-center mt-8 justify-center p-4 text-center pb-16">
             <Image src="/assets/logologin-footer-white.png" width={150} height={100} alt="Logo Login" />
             <span className="mt-4 text-sm">LOGIN INFORMÁTICA COMÉRCIO E REPRESENTAÇÃO LTDA
 <br /> CNPJ: 00.066.716/0001-91</span>
